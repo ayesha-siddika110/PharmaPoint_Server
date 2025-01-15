343956520edfbfa13ea3039ac96c101e9b746ece
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2025-01-15 08:27:34 +0600

    verify token

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+var jwt = require('jsonwebtoken');
+
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.331jm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const usersCollections = client.db("PharmaPoint_DB").collection("users")
+
+
+    // //sent jwt 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_Token, {
+        expiresIn: '10h'
+      })
+      // console.log(token)
+      res.send({ token })
+    })
+    //verifyToken midddleware
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verifyToken ', req.headers.authorization)
+      if (!req.headers.authorization) {
+        return res.send(401).send({ message: 'unAuthorized access' })
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_Token, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unAuthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollections.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    // get the users
+
+    app.get('/users',verifyToken,verifyAdmin, async (req, res) => {
+      const result = await usersCollections.find().toArray()
+      res.send(result)
+    })
+
+    //post users data
+    app.post('/users', async (req, res) => {
+      const newuser = req.body;
+      const query = { email: newuser.email }
+      const existingUser = await usersCollections.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'User data already exist', insertedId: null })
+      }
+      const result = await usersCollections.insertOne(newuser)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
